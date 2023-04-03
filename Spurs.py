476d6e0fc77ca4026dcc035734b0a0cebd003623
Joey Chapa <name@domain.examplejoeychapa@yahoo.com>
2023-04-03 19:06:50 +0000

    Spurs_NEw

---
+++ Spurs.py
@@ -0,0 +1,9 @@
+sa_spurs_wins1996=20
+sa_spurs_wins2023=20
+
+if sa_spurs_wins1996 > sa_spurs_wins2023:
+  print("1996-1997 Spurs are not the worst in Spurs history!")
+elif sa_spurs_wins2023 == sa_spurs_wins1996:
+  print(" Tied for the worst record since the Spurs of 1996")
+else:
+  print(" Not the worst record in Spurs history!! ")
